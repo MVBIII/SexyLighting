5ebd3952aeeb36951f707e8986097c90a4efc426
Sir Winkerly Nuzzlebottom <noah.j.eutsler@gmail.com>
2022-01-08 14:47:39 -0500

    color test

--- main.py
+++ main.py
@@ -12,21 +12,21 @@ LED_ORDER = neopixel.RGB # order of LED colours. May also be RGB, GRBW, or RGBW
 strip = neopixel.NeoPixel(LED_PIN, LED_COUNT, brightness = LED_BRIGHTNESS, auto_write=False, pixel_order = LED_ORDER)
 strip.fill((255,0,0))
 strip.show()
-red=5
-green=5
-blue=5
+red= 97
+green=93
+blue=182
 redstep=1
 greenstep=2
 bluestep=3
-while True:
-    red+=redstep
-    green+=greenstep
-    blue+=bluestep
-    if red>250 or red<3:
-        redstep = -redstep
-    if green>250 or green<3:
-        greenstep = -greenstep
-    if blue>250 or blue<3:
-        bluestep = -bluestep
-    strip.fill((0, green , blue))
-    strip.show()
+# while True:
+#     red+=redstep
+#     green+=greenstep
+#     blue+=bluestep
+#     if red>250 or red<3:
+#         redstep = -redstep
+#     if green>250 or green<3:
+#         greenstep = -greenstep
+#     if blue>250 or blue<3:
+#         bluestep = -bluestep
+strip.fill((green, red , blue))
+strip.show()
